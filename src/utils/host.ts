0704746a17c88535c2b21c04fb522f0ec2f61538
Phips Peter <pspeter3@users.noreply.github.com>
2021-12-01 18:24:11 -0500

    Day1 (#2)
    * Add day 1 solution
    
    This is a basic solution with very few abstractions.
    
    * Update host
    
    Use a better solution format.

--- src/utils/host.ts
+++ src/utils/host.ts
@@ -1,51 +1,42 @@
 import fs from "fs/promises";
 import path from "path";
 
-/**
- * Reads input relative to the solution.
- * @param dirname The solution directory name.
- * @returns The file contents.
- */
-export async function readInput(dirname: string): Promise<string> {
-    return await fs.readFile(path.join(dirname, "input.txt"), "utf8");
+export interface Solution<I, O> {
+    setup(input: string): I;
+    part1(input: I): O;
+    part2?(input: I): O;
 }
 
 /**
- * A solution for part of a problem.
+ * Executes the solution if the module is main.
+ * @param mod The module.
+ * @param solution The solution.
  */
-export type Solution<T> = () => T;
-
-/**
- * Create an array of solutions.
- */
-export type SolutionFactory<T> = () => Promise<ReadonlyArray<Solution<T>>>;
-
-/**
- * Registers a factory as the main function.
- * @param mod The parent module to register.
- * @param factory The solution factory.
- */
-export function main<T>(mod: NodeModule, factory: SolutionFactory<T>): void {
+export function main<I, O>(mod: NodeModule, solution: Solution<I, O>): void {
     if (require.main === mod) {
-        solve(factory).catch((err) => {
-            console.error(err);
-            process.exitCode = 1;
-        });
+        solve(path.dirname(mod.filename), solution).catch(exit);
     }
 }
 
-/**
- * Runs a set of solutions from a factory.
- * @param factory The factory for solutions.
- */
-async function solve<T>(factory: SolutionFactory<T>): Promise<void> {
-    console.time("Setup");
-    const solutions = await factory();
-    console.timeEnd("Setup");
-    for (const [index, solution] of solutions.entries()) {
-        const label = `Part ${index + 1}`;
-        console.time(label);
-        console.log(solution());
-        console.timeEnd(label);
+async function solve<I, O>(
+    dirname: string,
+    { setup, part1, part2 }: Solution<I, O>
+): Promise<void> {
+    const contents = await fs.readFile(path.join(dirname, "input.txt"), "utf8");
+    const input = time("Setup", setup, contents);
+    console.log(time("Part 1", part1, input));
+    if (part2) {
+        console.log(time("Part 2", part2, input));
     }
+}
+
+function time<A, R>(label: string, callback: (args: A) => R, args: A): R {
+    console.time(label);
+    const result = callback(args);
+    console.timeEnd(label);
+    return result;
+}
+function exit(err: Error): void {
+    console.error(err);
+    process.exitCode = 1;
 }
