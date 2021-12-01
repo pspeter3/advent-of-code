a669f4f0e3faca9332ebca09749a7c8abd13154e
Phips Peter <phips@asana.com>
2021-12-01 18:21:22 -0500

    Update host
    Use a better solution format.

--- src/2021/01/index.ts
+++ src/2021/01/index.ts
@@ -1,4 +1,13 @@
-import { main, readInput } from "../../utils/host";
+import { main } from "../../utils/host";
+
+const setup = (input: string): Uint16Array =>
+    Uint16Array.from(input.trim().split("\n"), (line, index) => {
+        const value = parseInt(line);
+        if (Number.isNaN(value)) {
+            throw new Error(`Invalid line ${index + 1}: ${line}`);
+        }
+        return value;
+    });
 
 const increasing = (values: Uint16Array): number =>
     values.reduce((sum, value, index) => {
@@ -8,33 +17,22 @@ const increasing = (values: Uint16Array): number =>
         return sum;
     }, 0);
 
-const part1 = (depths: Uint16Array) => (): number => increasing(depths);
+const part1 = (depths: Uint16Array): number => increasing(depths);
 
-const part2 = (depths: Uint16Array) => (): number => {
-    const windows = Uint16Array.from(
-        {
-            length: depths.length - 2,
-        },
-        (_, index) => {
-            let sum = 0;
-            for (let i = 0; i < 3; i++) {
-                sum += depths[index + i];
+const part2 = (depths: Uint16Array): number =>
+    increasing(
+        Uint16Array.from(
+            {
+                length: depths.length - 2,
+            },
+            (_, index) => {
+                let sum = 0;
+                for (let i = 0; i < 3; i++) {
+                    sum += depths[index + i];
+                }
+                return sum;
             }
-            return sum;
-        }
+        )
     );
-    return increasing(windows);
-};
 
-main(module, async () => {
-    const input = await readInput(__dirname);
-    const lines = input.trim().split("\n");
-    const depths = Uint16Array.from(lines, (line, index) => {
-        const value = parseInt(line);
-        if (Number.isNaN(value)) {
-            throw new Error(`Invalid line ${index + 1}: ${line}`);
-        }
-        return value;
-    });
-    return [part1(depths), part2(depths)];
-});
+main<Uint16Array, number>(module, { setup, part1, part2 });
