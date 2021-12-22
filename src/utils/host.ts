d78ad2389c3eb2858534cf0550e0272d0ff49829
Phips Peter <pspeter3@users.noreply.github.com>
2021-12-22 14:19:13 -0500

    Add examples support (#29)
    Allow running examples so that way I don't need to keep changing filenames.

--- src/utils/host.ts
+++ src/utils/host.ts
@@ -16,21 +16,37 @@ export function main<T>(
     ...solutions: Solution<T>[]
 ): void {
     if (require.main === mod) {
-        console.time("Load");
-        const input = fs.readFileSync(
-            path.join(path.dirname(mod.filename), "input.txt"),
-            "utf8"
-        );
-        console.timeEnd("Load");
-        console.time("Parse");
-        const data = schema.parse(input);
-        console.timeEnd("Parse");
-        for (const [index, solution] of solutions.entries()) {
-            const label = `Part ${index + 1}`;
-            console.time(label);
-            const result = solution(data);
-            console.timeEnd(label);
-            console.log(result);
-        }
+        exec("Example", mod, schema, solutions);
+        exec("Input", mod, schema, solutions);
     }
+}
+
+function exec<T>(
+    group: string,
+    mod: NodeModule,
+    schema: ZodType<T, ZodTypeDef, T>,
+    solutions: Solution<T>[]
+): void {
+    const filename = path.join(
+        path.dirname(mod.filename),
+        `${group.toLowerCase()}.txt`
+    );
+    if (!fs.existsSync(filename)) {
+        return;
+    }
+    console.group(group);
+    console.time("Load");
+    const input = fs.readFileSync(filename, "utf8");
+    console.timeEnd("Load");
+    console.time("Parse");
+    const data = schema.parse(input);
+    console.timeEnd("Parse");
+    for (const [index, solution] of solutions.entries()) {
+        const label = `Part ${index + 1}`;
+        console.time(label);
+        const result = solution(data);
+        console.timeEnd(label);
+        console.log(result);
+    }
+    console.groupEnd();
 }
