aa594bbda1ce2c4beb8f92f062e4780b0a1c5ec2
Phips Peter <pspeter3@users.noreply.github.com>
2022-12-04 00:25:02 -0800

    2022-04 (#38)
    Had to remeber how ranges work
    https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-if-two-ranges-overlap

---
+++ src/2022/04/index.ts
@@ -0,0 +1,30 @@
+import { boolean, z } from "zod";
+import { main } from "../../utils/host";
+import { IntSchema, LinesSchema, StringSchema } from "../../utils/schemas";
+
+const RangeSchema = z.preprocess(
+    (section) => StringSchema.parse(section).split("-"),
+    z.tuple([IntSchema, IntSchema])
+);
+const PairSchema = z.preprocess(
+    (line) => StringSchema.parse(line).split(","),
+    z.tuple([RangeSchema, RangeSchema])
+);
+const schema = LinesSchema(PairSchema);
+
+type Range = readonly [min: number, max: number];
+type Pair = readonly [a: Range, b: Range];
+
+const contains = ([aMin, aMax]: Range, [bMin, bMax]: Range): boolean =>
+    aMin <= bMin && aMax >= bMax;
+
+const overlap = ([aMin, aMax]: Range, [bMin, bMax]: Range): boolean =>
+    aMin <= bMax && bMin <= aMax;
+
+const part1 = (input: ReadonlyArray<Pair>): number =>
+    input.filter(([a, b]) => contains(a, b) || contains(b, a)).length;
+
+const part2 = (input: ReadonlyArray<Pair>): number =>
+    input.filter(([a, b]) => overlap(a, b)).length;
+
+main(module, (input) => schema.parse(input), part1, part2);
