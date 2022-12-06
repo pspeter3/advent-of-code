086444d5415b62c839d72f10a4da971e60066f54
Phips Peter <pspeter3@users.noreply.github.com>
2022-12-06 13:27:47 -0800

    2022-06 (#40)

---
+++ src/2022/06/index.ts
@@ -0,0 +1,17 @@
+import { main } from "../../utils/host";
+
+const analyze = (input: string, packet: number): number => {
+    for (let i = 0; i < input.length - packet; i++) {
+        const seen = new Set(input.slice(i, i + packet));
+        if (seen.size === packet) {
+            return i + packet;
+        }
+    }
+    return -1;
+};
+
+const part1 = (input: string): number => analyze(input, 4);
+
+const part2 = (input: string): number => analyze(input, 14);
+
+main(module, (input) => input.trim(), part1, part2);
