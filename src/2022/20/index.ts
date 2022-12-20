cad52726cdec31070acb0d68cd35cf4ded7ad235
Phips Peter <pspeter3@users.noreply.github.com>
2022-12-20 07:45:30 -0800

    2022-20 (#57)

---
+++ src/2022/20/index.ts
@@ -0,0 +1,48 @@
+import { main } from "../../utils/host";
+import { IntSchema, LinesSchema } from "../../utils/schemas";
+
+const schema = LinesSchema(IntSchema);
+
+const parse = (input: string): ReadonlyArray<number> => schema.parse(input);
+
+interface Position {
+    readonly value: number;
+    readonly index: number;
+}
+
+const mix = (list: Array<Position>): void => {
+    for (let i = 0; i < list.length; i++) {
+        const index = list.findIndex(({ index }) => index === i);
+        const [element] = list.splice(index, 1);
+        list.splice((index + element.value) % list.length, 0, element);
+    }
+};
+
+const coordinates = (list: ReadonlyArray<Position>): number => {
+    const start = list.findIndex(({ value }) => value === 0);
+    return [1000, 2000, 3000].reduce((sum, offset) => {
+        return sum + list[(start + offset) % list.length].value;
+    }, 0);
+};
+
+const part1 = (sequence: ReadonlyArray<number>): number => {
+    let list: Position[] = sequence.map((value, index) => ({
+        value,
+        index,
+    }));
+    mix(list);
+    return coordinates(list);
+};
+
+const part2 = (sequence: ReadonlyArray<number>): number => {
+    let list: Position[] = sequence.map((value, index) => ({
+        value: value * 811589153,
+        index,
+    }));
+    for (let i = 0; i < 10; i++) {
+        mix(list);
+    }
+    return coordinates(list);
+};
+
+main(module, parse, part1, part2);
