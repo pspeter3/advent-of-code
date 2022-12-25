cc686dce7b29daa9c625fc72c2f973d8f8c301c7
Phips Peter <pspeter3@users.noreply.github.com>
2022-12-25 00:36:56 -0500

    2022-25 (#66)
    Had to go to Reddit but learned about quniary systems

---
+++ src/2022/25/index.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+import { main } from "../../utils/host";
+import { LinesSchema } from "../../utils/schemas";
+
+const DIGITS = ["0", "1", "2", "=", "-"] as const;
+const DigitSchema = z.enum(DIGITS);
+type Digit = typeof DIGITS[number];
+const VALUES = {
+    "=": -2,
+    "-": -1,
+    "0": 0,
+    "1": 1,
+    "2": 2,
+} as const;
+
+type SNAFU = ReadonlyArray<Digit>;
+
+const toDecimal = (snafu: SNAFU): number =>
+    snafu.reduce(
+        (sum, digit, index) =>
+            sum + VALUES[digit] * Math.pow(5, snafu.length - index - 1),
+        0
+    );
+
+const toSNAFU = (value: number): string => {
+    if (value === 0) {
+        return "";
+    }
+    return toSNAFU(Math.floor((value + 2) / 5)) + DIGITS[value % 5];
+};
+
+const schema = LinesSchema(
+    z
+        .string()
+        .transform((line) => line.split(""))
+        .pipe(z.array(DigitSchema))
+);
+
+const parse = (input: string): ReadonlyArray<SNAFU> => schema.parse(input);
+
+const part1 = (snafus: ReadonlyArray<SNAFU>): string => toSNAFU(snafus.reduce((sum, snafu) => sum + toDecimal(snafu), 0));
+
+main(module, parse, part1);
