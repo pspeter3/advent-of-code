59e28bc6023c0f701b320a8aec46899fcda5280b
Phips Peter <phips@asana.com>
2023-11-06 11:05:49 -0500

    Add basic testing

---
+++ src/common/math.ts
@@ -0,0 +1,8 @@
+/**
+ * Converts a number to the triangle number equivalent.
+ * @param value The value to convert
+ * @returns The triangle numbeer
+ */
+export function toTriangle(value: number): number {
+    return 0.5 * value * (value + 1);
+}
