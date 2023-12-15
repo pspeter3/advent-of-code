aaf4c3b79df791f7599308740821d022873ba15f
Phips Peter <pspeter3@users.noreply.github.com>
2023-12-15 08:00:13 -0500

    2023 15 (#87)
    * 2023-15
    
    Today was a lot of fun and easier than the previous days.
    
    * Add enumerate

---
+++ src/2023/15/index.ts
@@ -0,0 +1,75 @@
+import { enumerate, map, sum } from "../../common/itertools";
+import { main } from "../../utils/host";
+
+const REMOVE = "-";
+const ADD = "=";
+
+interface RemoveOperation {
+    readonly kind: "-";
+    readonly label: string;
+}
+
+interface AddOperation {
+    readonly kind: "=";
+    readonly label: string;
+    readonly focal: number;
+}
+
+type Operation = RemoveOperation | AddOperation;
+
+const hash = (label: string): number =>
+    Array.from(label).reduce(
+        (value, char) => ((value + char.charCodeAt(0)) * 17) % 256,
+        0,
+    );
+
+function toOperation(step: string): Operation {
+    if (step.endsWith(REMOVE)) {
+        return {
+            kind: REMOVE,
+            label: step.replace(REMOVE, ""),
+        };
+    }
+    const [label, value] = step.split(ADD);
+    return { kind: ADD, label, focal: parseInt(value, 10) };
+}
+
+function* toPower(
+    boxes: ReadonlyArray<ReadonlyMap<string, number>>,
+): Iterable<number> {
+    for (const [index, box] of boxes.entries()) {
+        for (const [offset, focal] of enumerate(box.values())) {
+            yield (index + 1) * (offset + 1) * focal;
+        }
+    }
+}
+
+const parse = (input: string): ReadonlyArray<string> => input.trim().split(",");
+
+const part1 = (sequence: ReadonlyArray<string>): number =>
+    sum(map(sequence, hash));
+
+const part2 = (sequence: ReadonlyArray<string>): number => {
+    const boxes: ReadonlyArray<Map<string, number>> = Array.from(
+        { length: 256 },
+        () => new Map(),
+    );
+    for (const step of sequence) {
+        const op = toOperation(step);
+        const index = hash(op.label);
+        const box = boxes[index];
+        switch (op.kind) {
+            case REMOVE: {
+                box.delete(op.label);
+                break;
+            }
+            case ADD: {
+                box.set(op.label, op.focal);
+                break;
+            }
+        }
+    }
+    return sum(toPower(boxes));
+};
+
+main(module, parse, part1, part2);
