d617a36dae1286fdf95a15c618aeb92f9608f52d
Phips Peter <phips@asana.com>
2023-12-16 10:52:41 -0500

    Add Grid2D
    Use a new helper library

--- src/2023/16/index.ts
+++ src/2023/16/index.ts
@@ -2,84 +2,25 @@ import z from "zod";
 import { main } from "../../utils/host";
 import { LinesSchema } from "../../utils/schemas";
 import { map, max } from "../../common/itertools";
+import {
+    CardinalDirection,
+    GridBounds2D,
+    GridVector2D,
+    GridVector2DRecord,
+    MatrixGrid,
+    toGridDelta,
+    toGridDirection,
+} from "../../common/grid2d";
 
 type Mirror = "\\" | "/";
 type Splitter = "|" | "-";
 type Empty = ".";
 type Tile = Mirror | Splitter | Empty;
-type TileList = ReadonlyArray<Tile>;
-type TileMatrix = ReadonlyArray<TileList>;
 
-interface GridVector {
-    readonly q: number;
-    readonly r: number;
-}
+const isHorizontal = (delta: GridVector2DRecord): boolean => delta.r === 0;
+const isVertical = (delta: GridVector2DRecord): boolean => delta.q === 0;
 
-class Grid {
-    readonly bounds: GridVector;
-    readonly #matrix: TileMatrix;
-
-    constructor(matrix: TileMatrix) {
-        this.bounds = { q: matrix[0].length, r: matrix.length };
-        this.#matrix = matrix;
-    }
-
-    has({ q, r }: GridVector): boolean {
-        return q >= 0 && q < this.bounds.q && r >= 0 && r < this.bounds.r;
-    }
-
-    at(vector: GridVector): Tile {
-        if (!this.has(vector)) {
-            throw new Error("Out of bounds");
-        }
-        const { q, r } = vector;
-        return this.#matrix[r][q];
-    }
-
-    toId(vector: GridVector): number {
-        if (!this.has(vector)) {
-            throw new Error("Out of bounds");
-        }
-        const { q, r } = vector;
-        return r * this.bounds.q + q;
-    }
-}
-
-enum Direction {
-    Up = 0,
-    Right = 1,
-    Down = 2,
-    Left = 3,
-}
-
-const DELTAS: ReadonlyArray<GridVector> = [
-    { q: 0, r: -1 },
-    { q: 1, r: 0 },
-    { q: 0, r: 1 },
-    { q: -1, r: 0 },
-];
-
-const add = (a: GridVector, b: GridVector): GridVector => ({
-    q: a.q + b.q,
-    r: a.r + b.r,
-});
-
-const equals = (a: GridVector, b: GridVector): boolean =>
-    a.q === b.q && a.r === b.r;
-
-const toDelta = (direction: Direction): GridVector => DELTAS[direction];
-function toDirection(delta: GridVector): Direction {
-    const index = DELTAS.findIndex((d) => equals(delta, d));
-    if (index === -1) {
-        throw new Error("Invalid delta");
-    }
-    return index as Direction;
-}
-
-const isHorizontal = (delta: GridVector): boolean => delta.r === 0;
-const isVertical = (delta: GridVector): boolean => delta.q === 0;
-
-type Beam = readonly [position: GridVector, direction: Direction];
+type Beam = readonly [position: GridVector2D, direction: CardinalDirection];
 
 const TileSchema = z.enum(["\\", "/", "|", "-", "."]);
 const TileListSchema = z
@@ -87,13 +28,17 @@ const TileListSchema = z
     .transform((line) => line.split(""))
     .pipe(z.array(TileSchema));
 const Schema = LinesSchema(TileListSchema).transform(
-    (matrix) => new Grid(matrix),
+    (matrix) => new MatrixGrid(matrix),
 );
 
-function energize(grid: Grid, start: Beam): number {
+function energize(grid: MatrixGrid<Tile>, start: Beam): number {
     const queue: Beam[] = [start];
-    const cache = new Map<number, Set<Direction>>();
-    const check = (id: number, direction: Direction): boolean => {
+    const cache = new Map<number, Set<CardinalDirection>>();
+    const check = (
+        position: GridVector2DRecord,
+        direction: CardinalDirection,
+    ): boolean => {
+        const id = grid.bounds.toId(position);
         if (cache.get(id)?.has(direction)) {
             return true;
         }
@@ -104,12 +49,12 @@ function energize(grid: Grid, start: Beam): number {
         return false;
     };
     for (const [position, direction] of queue) {
-        const delta = toDelta(direction);
-        const next = add(position, delta);
-        if (!grid.has(next) || check(grid.toId(next), direction)) {
+        const delta = toGridDelta(direction);
+        const next = position.add(delta);
+        if (!grid.bounds.contains(next) || check(next, direction)) {
             continue;
         }
-        const push = (direction: Direction): unknown =>
+        const push = (direction: CardinalDirection): unknown =>
             queue.push([next, direction]);
         switch (grid.at(next)) {
             case ".": {
@@ -118,18 +63,18 @@ function energize(grid: Grid, start: Beam): number {
             }
             case "\\": {
                 const { q, r } = delta;
-                push(toDirection({ q: r, r: q }));
+                push(toGridDirection({ q: r, r: q }) as CardinalDirection);
                 break;
             }
             case "/": {
                 const { q, r } = delta;
-                push(toDirection({ q: -r, r: -q }));
+                push(toGridDirection({ q: -r, r: -q }) as CardinalDirection);
                 break;
             }
             case "|": {
                 if (isHorizontal(delta)) {
-                    push(Direction.Up);
-                    push(Direction.Down);
+                    push(CardinalDirection.North);
+                    push(CardinalDirection.South);
                 } else {
                     push(direction);
                 }
@@ -137,8 +82,8 @@ function energize(grid: Grid, start: Beam): number {
             }
             case "-": {
                 if (isVertical(delta)) {
-                    push(Direction.Right);
-                    push(Direction.Left);
+                    push(CardinalDirection.East);
+                    push(CardinalDirection.West);
                 } else {
                     push(direction);
                 }
@@ -149,23 +94,23 @@ function energize(grid: Grid, start: Beam): number {
     return cache.size;
 }
 
-function* starts(grid: Grid): Iterable<Beam> {
-    for (let q = 0; q < grid.bounds.q; q++) {
-        yield [{ q, r: -1 }, Direction.Down];
-        yield [{ q, r: grid.bounds.r }, Direction.Up];
+function* starts({ min, max }: GridBounds2D): Iterable<Beam> {
+    for (let q = min.q; q < max.q; q++) {
+        yield [new GridVector2D(q, -1), CardinalDirection.South];
+        yield [new GridVector2D(q, max.r), CardinalDirection.North];
     }
-    for (let r = 0; r < grid.bounds.r; r++) {
-        yield [{ q: -1, r }, Direction.Right];
-        yield [{ q: grid.bounds.q, r }, Direction.Left];
+    for (let r = min.r; r < max.r; r++) {
+        yield [new GridVector2D(-1, r), CardinalDirection.East];
+        yield [new GridVector2D(max.q, r), CardinalDirection.West];
     }
 }
 
-const parse = (input: string): Grid => Schema.parse(input);
+const parse = (input: string): MatrixGrid<Tile> => Schema.parse(input);
 
-const part1 = (grid: Grid): number =>
-    energize(grid, [{ q: -1, r: 0 }, Direction.Right]);
+const part1 = (grid: MatrixGrid<Tile>): number =>
+    energize(grid, [new GridVector2D(-1, 0), CardinalDirection.East]);
 
-const part2 = (grid: Grid): number =>
-    max(map(starts(grid), (beam) => energize(grid, beam)));
+const part2 = (grid: MatrixGrid<Tile>): number =>
+    max(map(starts(grid.bounds), (beam) => energize(grid, beam)));
 
 main(module, parse, part1, part2);
---
+++ src/common/grid2d.ts
@@ -0,0 +1,250 @@
+import { map } from "./itertools";
+
+export interface GridVector2DRecord {
+    readonly q: number;
+    readonly r: number;
+}
+
+export type GridVector2DTuple = readonly [q: number, r: number];
+
+export enum CardinalDirection {
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3,
+}
+
+export enum DiagonalDirection {
+    NorthEast = 4,
+    SouthEast = 5,
+    SouthWest = 6,
+    NorthWest = 7,
+}
+
+export type GridDirection = CardinalDirection | DiagonalDirection;
+
+const DELTAS: ReadonlyArray<GridVector2DRecord> = [
+    { q: 0, r: -1 },
+    { q: 1, r: 0 },
+    { q: 0, r: 1 },
+    { q: -1, r: 0 },
+    { q: 1, r: -1 },
+    { q: 1, r: 1 },
+    { q: 1, r: 1 },
+    { q: -1, r: 1 },
+];
+
+export function toGridDelta(direction: GridDirection): GridVector2DRecord {
+    return DELTAS[direction];
+}
+
+export function toGridDirection(delta: GridVector2DRecord): GridDirection {
+    const index = DELTAS.findIndex(
+        ({ q, r }) => q === delta.q && r === delta.r,
+    );
+    if (index === -1) {
+        throw new Error("Invalid delta");
+    }
+    return index;
+}
+
+export function isCardinalDirection(
+    direction: GridDirection,
+): direction is CardinalDirection {
+    return direction < 4;
+}
+
+export function isDiagonalDiection(
+    direction: GridDirection,
+): direction is DiagonalDirection {
+    return direction >= 4;
+}
+
+export class GridVector2D implements GridVector2DRecord {
+    static readonly ORIGIN = new GridVector2D(0, 0);
+
+    static fromRecord({ q, r }: GridVector2DRecord): GridVector2D {
+        return new GridVector2D(q, r);
+    }
+
+    static fromTuple([q, r]: GridVector2DTuple): GridVector2D {
+        return new GridVector2D(q, r);
+    }
+
+    readonly q: number;
+    readonly r: number;
+
+    constructor(q: number, r: number) {
+        this.q = q;
+        this.r = r;
+    }
+
+    add({ q, r }: GridVector2DRecord): GridVector2D {
+        return new GridVector2D(this.q + q, this.r + r);
+    }
+
+    equals({ q, r }: GridVector2DRecord): boolean {
+        return this.q === q && this.r === r;
+    }
+
+    distance({ q, r }: GridVector2DRecord): number {
+        return Math.abs(q - this.q) + Math.abs(r - this.r);
+    }
+
+    neighbor(direction: GridDirection): GridVector2D {
+        return this.add(toGridDelta(direction));
+    }
+
+    north(): GridVector2D {
+        return this.neighbor(CardinalDirection.North);
+    }
+
+    east(): GridVector2D {
+        return this.neighbor(CardinalDirection.East);
+    }
+
+    south(): GridVector2D {
+        return this.neighbor(CardinalDirection.South);
+    }
+
+    west(): GridVector2D {
+        return this.neighbor(CardinalDirection.West);
+    }
+
+    northEast(): GridVector2D {
+        return this.neighbor(DiagonalDirection.NorthEast);
+    }
+
+    southEast(): GridVector2D {
+        return this.neighbor(DiagonalDirection.SouthEast);
+    }
+
+    southWest(): GridVector2D {
+        return this.neighbor(DiagonalDirection.SouthWest);
+    }
+
+    northWest(): GridVector2D {
+        return this.neighbor(DiagonalDirection.NorthWest);
+    }
+
+    *neighbors(): Iterable<GridVector2D> {
+        yield this.north();
+        yield this.east();
+        yield this.south();
+        yield this.west();
+    }
+
+    *diagonals(): Iterable<GridVector2D> {
+        yield this.northEast();
+        yield this.southEast();
+        yield this.southWest();
+        yield this.northWest();
+    }
+}
+
+export class GridBounds2D implements Iterable<GridVector2D> {
+    readonly min: GridVector2D;
+    readonly max: GridVector2D;
+
+    static fromOrigin(dimensions: GridVector2DRecord): GridBounds2D {
+        return new GridBounds2D(GridVector2D.ORIGIN, dimensions);
+    }
+
+    constructor(a: GridVector2DRecord, b: GridVector2DRecord) {
+        this.min = new GridVector2D(Math.min(a.q, b.q), Math.min(a.r, b.r));
+        this.max = new GridVector2D(Math.max(a.q, b.q), Math.max(a.r, b.r));
+    }
+
+    get cols(): number {
+        return this.max.q - this.min.q;
+    }
+
+    get rows(): number {
+        return this.max.r - this.min.r;
+    }
+
+    assert(vector: GridVector2DRecord): void {
+        if (!this.contains(vector)) {
+            throw new Error("Out of bounds");
+        }
+    }
+
+    contains({ q, r }: GridVector2DRecord): boolean {
+        return (
+            q >= this.min.q &&
+            q < this.max.q &&
+            r >= this.min.r &&
+            r < this.max.r
+        );
+    }
+
+    toLocal({ q, r }: GridVector2DRecord): GridVector2D {
+        return new GridVector2D(q - this.min.q, r - this.min.r);
+    }
+
+    toGlobal({ q, r }: GridVector2DRecord): GridVector2D {
+        return new GridVector2D(q + this.min.q, r + this.min.r);
+    }
+
+    toId(vector: GridVector2DRecord): number {
+        this.assert(vector);
+        const { cols } = this;
+        const { q, r } = this.toLocal(vector);
+        return r * cols + q;
+    }
+
+    fromId(id: number): GridVector2D {
+        const { cols } = this;
+        const q = id % cols;
+        const r = Math.floor(id / cols);
+        const result = this.toGlobal({ q, r });
+        this.assert(result);
+        return result;
+    }
+
+    *[Symbol.iterator](): IterableIterator<GridVector2D> {
+        for (let q = this.min.q; q < this.max.q; q++) {
+            for (let r = this.min.r; r < this.max.r; r++) {
+                yield new GridVector2D(q, r);
+            }
+        }
+    }
+
+    equals(bounds: GridBounds2D): boolean {
+        return this.min.equals(bounds.min) && this.max.equals(bounds.max);
+    }
+}
+
+export type MatrixGridEntry<T> = readonly [vector: GridVector2D, value: T];
+
+export class MatrixGrid<T> implements Iterable<MatrixGridEntry<T>> {
+    readonly matrix: ReadonlyArray<ReadonlyArray<T>>;
+    readonly bounds: GridBounds2D;
+
+    constructor(matrix: ReadonlyArray<ReadonlyArray<T>>) {
+        this.matrix = matrix;
+        this.bounds = GridBounds2D.fromOrigin({
+            q: matrix[0].length,
+            r: matrix.length,
+        });
+    }
+
+    at(vector: GridVector2DRecord): T {
+        this.bounds.assert(vector);
+        return this.matrix[vector.r][vector.q];
+    }
+
+    keys(): Iterable<GridVector2D> {
+        return this.bounds[Symbol.iterator]();
+    }
+
+    values(): Iterable<T> {
+        return map(this.keys(), (vector) => this.at(vector));
+    }
+
+    *[Symbol.iterator](): IterableIterator<MatrixGridEntry<T>> {
+        for (const vector of this.keys()) {
+            yield [vector, this.at(vector)];
+        }
+    }
+}
