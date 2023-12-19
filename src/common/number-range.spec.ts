4565652bf69c0e8284d6c0fc1831f18783d7b17b
Phips Peter <pspeter3@users.noreply.github.com>
2023-12-19 15:54:12 -0500

    Number range (#92)
    * NumberRange
    
    Add basic number range.
    
    * Add Number Range
    
    * Fix assert imports

---
+++ src/common/number-range.spec.ts
@@ -0,0 +1,44 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+import { NumberRange } from "./number-range";
+
+describe("NumberRange", () => {
+    it("should have the correct length", () => {
+        assert.equal(new NumberRange(1, 5).length, 4);
+    });
+
+    it("should support includes", () => {
+        const range = new NumberRange(1, 5);
+        assert.equal(range.includes(2), true);
+        assert.equal(range.includes(0), false);
+        assert.equal(range.includes(5), false);
+    });
+
+    it("should support intersection", () => {
+        assert.deepEqual(
+            new NumberRange(1, 5).intersection({ min: 2, max: 6 }),
+            new NumberRange(2, 5),
+        );
+        assert.deepEqual(
+            new NumberRange(1, 5).intersection({ min: 5, max: 10 }),
+            null,
+        );
+        assert.deepEqual(
+            new NumberRange(1, 5).intersection({ min: 0, max: 1 }),
+            null,
+        );
+    });
+
+    it("should support split", () => {
+        const range = new NumberRange(1, 5);
+        assert.throws(() => range.split(0));
+        assert.deepEqual(range.split(3), [
+            new NumberRange(1, 3),
+            new NumberRange(3, 5),
+        ]);
+    });
+
+    it("should support equals", () => {
+        assert.equal(new NumberRange(1, 5).equals({ min: 1, max: 5 }), true);
+    });
+});
---
+++ src/common/number-range.ts
@@ -0,0 +1,69 @@
+export interface NumberRangeRecord {
+    readonly min: number;
+    readonly max: number;
+}
+
+export type NumberRangeTuple = readonly [min: number, max: number];
+
+export type NumberRangeSplit = readonly [left: NumberRange, right: NumberRange];
+
+export class NumberRange implements NumberRangeRecord, Iterable<number> {
+    static inclusive(a: number, b: number): NumberRange {
+        return new NumberRange(Math.min(a, b), Math.max(a, b) + 1);
+    }
+
+    static fromRecord({ min, max }: NumberRangeRecord): NumberRange {
+        return new NumberRange(min, max);
+    }
+
+    static fromTuple([min, max]: NumberRangeTuple): NumberRange {
+        return new NumberRange(min, max);
+    }
+
+    static fromLength(start: number, length: number): NumberRange {
+        return new NumberRange(start, start + length);
+    }
+
+    readonly min: number;
+    readonly max: number;
+
+    constructor(a: number, b: number) {
+        this.min = Math.min(a, b);
+        this.max = Math.max(a, b);
+    }
+
+    get length(): number {
+        return this.max - this.min;
+    }
+
+    includes(value: number): boolean {
+        return this.min <= value && value < this.max;
+    }
+
+    intersection(other: NumberRangeRecord): NumberRange | null {
+        const min = Math.max(this.min, other.min);
+        const max = Math.min(this.max, other.max);
+        if (min >= max) {
+            return null;
+        }
+        return new NumberRange(min, max);
+    }
+
+    split(value: number): NumberRangeSplit {
+        if (!this.includes(value)) {
+            throw new Error("Invalid split value");
+        }
+        const { min, max } = this;
+        return [new NumberRange(min, value), new NumberRange(value, max)];
+    }
+
+    equals({ min, max }: NumberRangeRecord): boolean {
+        return this.min === min && this.max === max;
+    }
+
+    *[Symbol.iterator](): Iterator<number> {
+        for (let i = this.min; i < this.max; i++) {
+            yield i;
+        }
+    }
+}
