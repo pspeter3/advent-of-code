8685ff3f6e0cd54bf5c281beb8bb738a9a525216
Phips Peter <pspeter3@users.noreply.github.com>
2024-11-13 15:16:43 -0500

    2015 Plane Ride (#114)
    * 2015-01
    
    * Restore year

---
+++ src/2015/01/index.ts
@@ -0,0 +1,40 @@
+import z from "zod";
+import { main } from "../../utils/host";
+import { sum } from "../../common/itertools";
+
+enum Instruction {
+    Up = "(",
+    Down = ")",
+}
+
+type InstructionList = ReadonlyArray<Instruction>;
+
+const InstructionListSchema = z
+    .string()
+    .transform((line) => line.trim().split(""))
+    .pipe(z.array(z.nativeEnum(Instruction)));
+
+const parse = (input: string): InstructionList =>
+    InstructionListSchema.parse(input);
+
+const part1 = (instructions: InstructionList): number =>
+    sum(
+        instructions
+            .values()
+            .map((instruction) => (instruction === Instruction.Down ? -1 : 1)),
+    );
+
+const part2 = (instructions: InstructionList): number => {
+    let floor = 0;
+    let position = 0;
+    for (const instruction of instructions) {
+        floor += instruction === Instruction.Down ? -1 : 1;
+        position++;
+        if (floor === -1) {
+            return position;
+        }
+    }
+    throw new Error("Never entered basement");
+};
+
+main(module, parse, part1, part2);
