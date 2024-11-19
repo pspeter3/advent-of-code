5285889355ae2cef148cd3c95feb18c2991c0225
Phips Peter <phips@asana.com>
2024-11-19 09:01:46 -0500

    2015-04
    A lot of hashes

---
+++ src/2015/04/index.ts
@@ -0,0 +1,22 @@
+import crypto from "node:crypto";
+import { main } from "../../utils/host";
+
+function mine(key: string, size: number): number {
+    const prefix = Array.from({length: size}, () => 0).join("");
+    let i = 1;
+    while (true) {
+        const hash = crypto.createHash("md5").update(`${key}${i}`).digest("hex");
+        if (hash.startsWith(prefix)) {
+            return i;
+        }
+        i++;
+    }
+}
+
+const parse = (input: string): string => input.trim();
+
+const part1 = (key: string): number => mine(key, 5);
+
+const part2 = (key: string): number => mine(key, 6);
+
+main(module, parse, part1, part2);
