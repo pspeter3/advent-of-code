8857a317489a3adf50bbe850e2fa2e0b9b9f2fda
Phips Peter <phips@asana.com>
2024-11-20 10:12:48 -0500

    Add graph abstractions
    Closes #105
    Closes #106

--- src/2023/25/index.ts
+++ src/2023/25/index.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { main } from "../../utils/host";
 import { LinesSchema } from "../../utils/schemas";
+import { Graph, minCut, WeightedMatrixGraph } from "../../common/graph";
 
 const EdgeListSchema = z
     .string()
@@ -23,14 +24,12 @@ const GraphSchema = LinesSchema(EdgeListSchema).transform((entries) => {
     return graph;
 });
 
-type Graph<T> = ReadonlyMap<T, ReadonlySet<T>>;
-
-function toMatrix<T>(graph: Graph<T>): Matrix {
-    const keys = Array.from(graph.keys());
-    const matrix = Array.from(keys, (i) =>
-        Array.from(keys, (j) => (graph.get(i)!.has(j) ? 1 : 0)),
+function toMatrixGraph<T>(graph: Graph<T>): WeightedMatrixGraph<T> {
+    const nodes = new Set(graph.keys());
+    const matrix = Array.from(nodes, (i) =>
+        Array.from(nodes, (j) => (graph.get(i)!.has(j) ? 1 : 0)),
     );
-    return matrix;
+    return { nodes, matrix };
 }
 
 type Matrix = number[][];
@@ -40,49 +39,10 @@ interface MinCutResult {
     readonly nodes: ReadonlySet<number>;
 }
 
-function minCut(matrix: Matrix): MinCutResult {
-    let best: MinCutResult = { cuts: Infinity, nodes: new Set() };
-    const size = matrix.length;
-    const partitions = Array.from({ length: size }, (_, i) => new Set([i]));
-    for (let phase = 1; phase < size; phase++) {
-        const weights = Array.from(matrix[0]);
-        let s = 0,
-            t = 0;
-        for (let iteration = 0; iteration < size - phase; iteration++) {
-            weights[t] = -Infinity;
-            s = t;
-            let index: number | null = null;
-            let max = -Infinity;
-            for (const [i, v] of weights.entries()) {
-                if (v > max) {
-                    max = v;
-                    index = i;
-                }
-            }
-            t = index!;
-            for (let i = 0; i < size; i++) {
-                weights[i] += matrix[t][i];
-            }
-        }
-        const cuts = weights[t] - matrix[t][t];
-        if (cuts < best.cuts) {
-            best = { cuts, nodes: partitions[t] };
-        }
-        for (const id of partitions[t]) {
-            partitions[s].add(id);
-        }
-        for (let i = 0; i < size; i++) matrix[s][i] += matrix[t][i];
-        for (let i = 0; i < size; i++) matrix[i][s] = matrix[s][i];
-        matrix[0][t] = -Infinity;
-    }
-
-    return best;
-}
-
 const parse = (input: string): Graph<string> => GraphSchema.parse(input);
 
 const part1 = (graph: Graph<string>): number => {
-    const { cuts, nodes } = minCut(toMatrix(graph));
+    const { nodes } = minCut(toMatrixGraph(graph));
     const s = nodes.size;
     const t = graph.size - s;
     return s * t;
