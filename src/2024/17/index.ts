818a5e3ead19b851e5deefc515d055cd68e4a318
Phips Peter <pspeter3@users.noreply.github.com>
2024-12-17 11:16:12 -0500

    2024-17 (#139)
    Needed help from Reddit for Part 2 and switch to BigInt

---
+++ src/2024/17/index.ts
@@ -0,0 +1,193 @@
+import z from "zod";
+import { main } from "../../utils/host";
+
+type Register = "A" | "B" | "C";
+
+enum OpCode {
+    adv = 0,
+    bxl = 1,
+    bst = 2,
+    jnz = 3,
+    bxc = 4,
+    out = 5,
+    bdv = 6,
+    cdv = 7,
+}
+
+interface Debugger {
+    readonly registers: Readonly<Record<Register, bigint>>;
+    readonly program: ReadonlyArray<OpCode>;
+}
+
+const RegisterSchema = z.enum(["A", "B", "C"]);
+const RegisterInitSchema = z
+    .string()
+    .transform((line) => {
+        const match = line.match(/^Register (\w): (\d+)$/);
+        if (match === null) {
+            return match;
+        }
+        const [, register, value] = match;
+        return [register, value];
+    })
+    .pipe(z.tuple([RegisterSchema, z.coerce.bigint()]));
+const RegistersSchema = z
+    .string()
+    .transform((chunk) => chunk.split("\n"))
+    .pipe(z.array(RegisterInitSchema))
+    .transform(
+        (entries) => Object.fromEntries(entries) as Record<Register, bigint>,
+    );
+
+const ProgramSchema = z
+    .string()
+    .transform((line) =>
+        line
+            .split(": ")[1]
+            .trim()
+            .split(",")
+            .map((value) => parseInt(value, 10)),
+    )
+    .pipe(z.array(z.nativeEnum(OpCode)));
+
+const DebuggerSchema = z
+    .string()
+    .transform((input) => {
+        const [registers, program] = input.split("\n\n");
+        return { registers, program };
+    })
+    .pipe(z.object({ registers: RegistersSchema, program: ProgramSchema }));
+
+class Computer {
+    readonly #registers: Record<Register, bigint>;
+    readonly #program: ReadonlyArray<OpCode>;
+    #instruction = 0;
+    readonly #out: number[] = [];
+    readonly #log: boolean;
+
+    constructor(
+        registers: Record<Register, bigint>,
+        program: ReadonlyArray<OpCode>,
+        log: boolean = false,
+    ) {
+        this.#registers = registers;
+        this.#program = program;
+        this.#log = log;
+    }
+
+    exec(): ReadonlyArray<number> {
+        while (this.#instruction < this.#program.length) {
+            const op = this.#program[this.#instruction];
+            const value = this.#program[this.#instruction + 1];
+            if (this.#log) {
+                console.group(OpCode[op], value);
+                console.log(this.#instruction, this.#registers);
+            }
+            this.#eval(op, value);
+            if (this.#log) {
+                console.log(this.#instruction, this.#registers);
+                console.groupEnd();
+            }
+        }
+        return this.#out;
+    }
+
+    #eval(op: OpCode, value: number): void {
+        switch (op) {
+            case OpCode.adv: {
+                this.#divide("A", value);
+                break;
+            }
+            case OpCode.bxl: {
+                this.#registers.B = this.#registers.B ^ BigInt(value);
+                this.#advance();
+                break;
+            }
+            case OpCode.bst: {
+                this.#registers.B = this.#combo(value) % 8n;
+                this.#advance();
+                break;
+            }
+            case OpCode.jnz: {
+                if (this.#registers.A !== 0n) {
+                    this.#instruction = value;
+                    break;
+                }
+                this.#advance();
+                break;
+            }
+            case OpCode.bxc: {
+                this.#registers.B = this.#registers.B ^ this.#registers.C;
+                this.#advance();
+                break;
+            }
+            case OpCode.out: {
+                this.#out.push(Number(this.#combo(value) % 8n));
+                this.#advance();
+                break;
+            }
+            case OpCode.bdv: {
+                this.#divide("B", value);
+                break;
+            }
+            case OpCode.cdv: {
+                this.#divide("C", value);
+                break;
+            }
+        }
+    }
+
+    #combo(op: OpCode): bigint {
+        switch (op) {
+            case 4: {
+                return this.#registers.A;
+            }
+            case 5: {
+                return this.#registers.B;
+            }
+            case 6: {
+                return this.#registers.C;
+            }
+            case 7: {
+                throw new Error("Invalid op code");
+            }
+            default: {
+                return BigInt(op);
+            }
+        }
+    }
+
+    #advance(): void {
+        this.#instruction += 2;
+    }
+
+    #divide(register: Register, value: number): void {
+        this.#registers[register] = this.#registers.A >> this.#combo(value);
+        this.#advance();
+    }
+}
+
+const parse = (input: string): Debugger => DebuggerSchema.parse(input);
+
+const part1 = ({ registers, program }: Debugger): string =>
+    new Computer(structuredClone(registers), program).exec().join(",");
+
+const part2 = ({ registers, program }: Debugger): bigint => {
+    const frontier = new Set<bigint>([0n]);
+    for (const a of frontier) {
+        const max = a + 8n;
+        for (let A = a; A < max; A++) {
+            const out = new Computer({ ...registers, A }, program).exec();
+            const base = program.length - out.length;
+            if (out.every((value, index) => value === program[base + index])) {
+                if (base === 0) {
+                    return A;
+                }
+                frontier.add(A << 3n);
+            }
+        }
+    }
+    throw new Error("Answer not found");
+};
+
+main(module, parse, part1, part2);
