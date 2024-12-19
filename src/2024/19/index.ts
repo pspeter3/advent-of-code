8ff07d26e58b5246265494f535b65adc493145a4
Phips Peter <phips@asana.com>
2024-12-19 00:18:46 -0500

    2024-19
    Tried regex but went to Reddit for the memoization.

---
+++ src/2024/19/index.ts
@@ -0,0 +1,50 @@
+import z from "zod";
+import { main } from "../../utils/host";
+import { LinesSchema } from "../../utils/schemas";
+import { len, sum } from "../../common/itertools";
+import { memoize } from "../../common/functools";
+
+interface Puzzle {
+    readonly towels: ReadonlyArray<string>;
+    readonly designs: ReadonlyArray<string>;
+}
+
+const TowelSchema = z.string().regex(/^[wubrg]+$/);
+const TowelListSchema = z
+    .string()
+    .transform((value) => value.trim().split(", "))
+    .pipe(z.array(TowelSchema));
+const DesignListSchema = LinesSchema(TowelSchema);
+const PuzzleSchema = z
+    .string()
+    .transform((input) => {
+        const [towels, designs] = input.trim().split("\n\n");
+        return { towels, designs };
+    })
+    .pipe(z.object({ towels: TowelListSchema, designs: DesignListSchema }));
+
+const match = memoize(
+    (towels: ReadonlyArray<string>, design: string): number => {
+        if (design.length === 0) {
+            return 1;
+        }
+        return sum(
+            towels.map((towel) => {
+                if (!design.startsWith(towel)) {
+                    return 0;
+                }
+                return match(towels, design.slice(towel.length));
+            }),
+        );
+    },
+);
+
+const parse = (input: string): Puzzle => PuzzleSchema.parse(input);
+
+const part1 = ({ towels, designs }: Puzzle): number =>
+    len(designs.values().filter((design) => match(towels, design) > 0));
+
+const part2 = ({ towels, designs }: Puzzle): number =>
+    sum(designs.values().map((design) => match(towels, design)));
+
+main(module, parse, part1, part2);
