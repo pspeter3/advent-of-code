55f3642508abeed2591a28e4e7584cbe1ccfde67
Phips Peter <pspeter3@users.noreply.github.com>
2025-12-01 07:31:52 -0500

    Add mod (#165)
    Add a modular arithmetic helper

--- src/common/math.test.ts
+++ src/common/math.test.ts
@@ -5,6 +5,7 @@ import {
     fromZigZag,
     greatestCommonDivisor,
     leastCommonMultiple,
+    mod,
     toCantor,
     toTriangle,
     toZigZag,
@@ -43,5 +44,17 @@ describe("greatestCommonDivisor", () => {
 describe("leastCommonMultiple", () => {
     it("should work for Wikipedia examples", () => {
         assert.equal(leastCommonMultiple(21, 6), 42);
+    });
+});
+
+describe("mod", () => {
+    it("should work for positive numbers", () => {
+        assert.equal(mod(1, 8), 1);
+        assert.equal(mod(9, 8), 1);
+    });
+
+    it("should work for negative numbers", () => {
+        assert.equal(mod(-1, 8), 7);
+        assert.equal(mod(-9, 8), 7);
     });
 });
